2f128f01779eda5d1b19c9991054d775d90a6b8c
Samukelo Dev <godthenorth@gmail.com>
2025-07-26 17:32:46 +0200

    Define The Api Routes - Tested - !00% Working!!!

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,18 @@
 <?php
-use App\Http\Controllers\PostController;
+
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FunnelsController;
 
 Route::get('/', function () {
-    return view('API');
+    return view('VolvCRM API');
 });
 
+//Auth:
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']);
+
+//Funnels:
+Route::middleware('auth:sanctum')->apiResource('funnels', FunnelsController::class);
