98ccfd43b22136f8543d8f646b9d711e262f35f3
samukelok <samkelokay2@gmail.com>
2025-07-30 23:04:43 +0200

    Update Vite manifest, index.html, and API routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FunnelsController;
+use App\Http\Controllers\LeadsController;
+use App\Http\Controllers\LeadsStatusChangeController;
 
 Route::get('/', function () {
     return view('VolvCRM API');
@@ -16,3 +18,9 @@ Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']);
 
 //Funnels:
 Route::middleware('auth:sanctum')->apiResource('funnels', FunnelsController::class);
+
+// Leads
+Route::apiResource('leads', LeadsController::class);
+
+// Lead Status Changes
+Route::apiResource('lead-status-changes', LeadsStatusChangeController::class);
