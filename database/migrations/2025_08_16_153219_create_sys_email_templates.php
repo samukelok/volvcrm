a39ad882969f60d90cb375d7aa8706c1fdb34af7
samukelok <samkelokay2@gmail.com>
2025-08-17 18:34:53 +0200

    Updated: Sys Email Templates - Add img_preview Column To The Table

--- database/migrations/2025_08_16_153219_create_sys_email_templates.php
+++ database/migrations/2025_08_16_153219_create_sys_email_templates.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->longText('body_html'); // store HTML version
             $table->longText('body_text')->nullable(); // fallback plain text version
             $table->enum('category', ['welcome', 'follow_up', 'promo', 'reminder', 'newsletter'])->default('welcome');
+            $table->string('preview_img')->nullable();
             $table->boolean('is_default')->default(true); // mark system templates
             $table->timestamps();
             $table->softDeletes();
