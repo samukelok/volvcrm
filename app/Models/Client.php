f9702e5d1fc2fff3e15d4cd90738a96a43b015d9
samukelok <samkelokay2@gmail.com>
2025-08-21 12:12:50 +0200

    Relationship: Reverse Relationship Of Clients and SMTP Settings

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -47,4 +47,9 @@ class Client extends Model
     {
         return $this->hasMany(Invitation::class);
     }
+
+    public function smtpSettings(): BelongsToMany
+    {
+        return $this->belongsToMany(SMTPSetting::class);
+    }
 }
