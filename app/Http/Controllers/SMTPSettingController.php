003d6ea228b78b6fa5cf39ca389bb96503ac8c02
samukelok <samkelokay2@gmail.com>
2025-08-21 12:13:32 +0200

    Prepare To Process SMTP Settings With A Controller

---
+++ app/Http/Controllers/SMTPSettingController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SMTPSetting;
+use Illuminate\Http\Request;
+
+class SMTPSettingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SMTPSetting $sMTPSetting)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SMTPSetting $sMTPSetting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SMTPSetting $sMTPSetting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SMTPSetting $sMTPSetting)
+    {
+        //
+    }
+}
