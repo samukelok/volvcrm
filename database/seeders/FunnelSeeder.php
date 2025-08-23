3b49c0c5af147a6ea2f28541ecccbe1571ee1312
samukelok <samkelokay2@gmail.com>
2025-08-23 10:31:40 +0200

    Seeder Using Client | Email Templates | Funnel Factory | Funnel Step Factories

--- database/seeders/FunnelSeeder.php
+++ database/seeders/FunnelSeeder.php
@@ -2,16 +2,39 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Funnel;
+use App\Models\FunnelStep;
+use App\Models\EmailTemplate;
 
 class FunnelSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        $funnel = Funnel::factory()->create([
+            'title' => 'Demo Drip Funnel',
+            'goal' => 'Convert trial users to paid',
+        ]);
+
+        $templates = EmailTemplate::factory(3)->create();
+
+        $step1 = FunnelStep::factory()->create([
+            'funnel_id' => $funnel->id,
+            'step_order' => 1,
+            'delay_hours' => 0,
+            'name' => 'Welcome Email',
+        ]);
+
+        $step2 = FunnelStep::factory()->create([
+            'funnel_id' => $funnel->id,
+            'step_order' => 2,
+            'delay_hours' => 24,
+            'name' => 'Follow-up Email',
+        ]);
+
+        // Attach templates to steps
+        $step1->emailTemplates()->attach($templates[0]->id, ['order_in_step' => 1]);
+        $step2->emailTemplates()->attach($templates[1]->id, ['order_in_step' => 1]);
+        $step2->emailTemplates()->attach($templates[2]->id, ['order_in_step' => 2]);
     }
 }
